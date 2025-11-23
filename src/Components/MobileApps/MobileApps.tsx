b0a25a82e48153f97a0a3b0dbc3931d762d23706
Adithya kiran C B <adithyakiran021@gmail.com>
2025-11-23 20:01:39 +0530

    feat: update MobileApps component to support dark theme styling for text elements

--- src/Components/MobileApps/MobileApps.tsx
+++ src/Components/MobileApps/MobileApps.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link, NavLink } from "react-router";
+import { useTheme } from "../Context/Context";
 
 const MobileApps = () => {
+  const theme = useTheme()
+  const isDark = theme === 'dark'
   return (
     <div className="h-screen w-screen flex items-center flex-col gap-6 justify-center">
       <div className="flex flex-row gap-5">
         <Link to={"/mobileApps/roosnest"}>
           <div className=" h-28 w-28 items-center rounded-lg flex-col flex hover:scale-110 transition-all duration-600 ease-in-out cursor-pointer">
             <img src="/roosnest.png" className="w-full h-full" alt="" />
-            <p className="text-center text-[12px] text-gray-300 font-sans">
+            <p className={`text-center text-[12px] ${!isDark && '!text-black'} text-gray-300 font-sans`}>
               RoosNest
             </p>
           </div>
@@ -16,14 +19,14 @@ const MobileApps = () => {
         <Link to={"/mobileApps/yaskitchen"}>
           <div className=" h-28 w-28 items-center rounded-lg flex-col flex hover:scale-110 transition-all duration-600 ease-in-out cursor-pointer">
             <img src="/yaskitchen.png" className="w-full h-full" alt="" />
-            <p className="text-center text-[12px] text-gray-300 font-sans">
+            <p className={`text-center text-[12px] ${!isDark && '!text-black'} text-gray-300 font-sans`}>
               Yas Kitchen
             </p>
           </div>
         </Link>
       </div>
       <NavLink to={"/"}>
-        <h1 className="font-sans text-sm text-gray-200">Go Home</h1>
+        <h1 className={`font-sans text-sm text-gray-200  ${isDark ? "!text-[#F28F3B] drop-shadow-[0_0_40px_rgba(242,143,59,0.3)]" : "!text-[#ff391e] drop-shadow-[0_0_40px_rgba(255,57,30,0.2)]"} `}>Go Home</h1>
       </NavLink>
     </div>
   );
